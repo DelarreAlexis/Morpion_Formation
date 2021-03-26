d9bc70a4919511b94f3a6d2c6b5f92f349ebfd25
Alexis Delarre <adelarre@amo-technologies.com>
2021-03-26 17:10:18 +0100

    MORPION

---
+++ MorpionFormation2/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorpionFormation2
+{
+	public partial class MainPage : ContentPage
+	{
+		private Button[] boutons = new Button[9];
+		private MoteurMorpion _jeu = new MoteurMorpion();
+
+		public MainPage()
+		{
+			InitializeComponent();
+			boutons[0] = btn1;
+			boutons[1] = btn2;
+			boutons[2] = btn3;
+			boutons[3] = btn4;
+			boutons[4] = btn5;
+			boutons[5] = btn6;
+			boutons[6] = btn7;
+			boutons[7] = btn8;
+			boutons[8] = btn9;
+		}
+
+		private void btn1_Clicked(object sender, EventArgs e)
+		{
+			_jeu.SetButton((Button)sender);
+			if (_jeu.CheckWinner(boutons))
+			{
+				lblEtatPartie.Text = string.Format("{0} a gagné la partie",
+					_jeu.PlayerTurn == 1 ? "Joueur 1" : "Joueur 2");
+			}
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			_jeu.ResetGame(boutons);
+			lblEtatPartie.Text = "";
+		}
+	}
+}
---
+++ MorpionFormation2/MoteurMorpion.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MorpionFormation2
+{
+	public class MoteurMorpion
+	{
+		private int _playerTurn;
+		public int PlayerTurn{get =>  _playerTurn ; set { _playerTurn = value; } }
+
+		private int[,] _winners = new int[,]
+		{
+			{0,1,2 },
+			{3,4,5 },
+			{6,7,8 },
+			{0,3,6 },
+			{1,4,7 },
+			{2,5,8 },
+			{0,4,8 },
+			{2,4,6 }
+		};
+
+		public bool CheckWinner(Button[] buttons)
+		{
+			bool gameOver = false;
+			for (int i = 0; i < 8; i++)
+			{
+				int a = _winners[i, 0];
+				int b = _winners[i, 1];
+				int c = _winners[i, 2];
+
+				Button b1 = buttons[a];
+				Button b2 = buttons[b];
+				Button b3 = buttons[c];
+
+				if (b1.Text == "" || b2.Text == "" || b3.Text == "")
+					continue;
+
+				if (b1.Text == b2.Text && b2.Text == b3.Text)
+				{
+					b1.BackgroundColor = Color.Red;
+					b2.BackgroundColor = Color.Red;
+					b3.BackgroundColor = Color.Red;
+					gameOver = true;
+					break;
+				}
+			}
+
+			bool isTie = true;
+			if (!gameOver)
+			{
+				foreach (Button b in buttons)
+				{
+					if (b.Text == "")
+					{
+						isTie = false;
+						break;
+					}
+				}
+				if (isTie)
+					gameOver = true;
+			}
+				return gameOver;		
+		}
+
+		public void SetButton(Button b)
+		{
+			if (b.Text == "")
+			{
+				b.Text = PlayerTurn == 1 ? "X" : "O";
+				/*if (PlayerTurn == 1)
+					b.Text = "X";
+				else
+					b.Text = "O";*/
+
+				PlayerTurn = PlayerTurn == 1 ? 2 : 1;
+				/*if (PlayerTurn == 1)
+					PlayerTurn = 2;
+				else
+					PlayerTurn = 1;*/
+			}
+		}
+
+		public void ResetGame(Button[] buttons)
+		{
+			PlayerTurn = 1;
+			foreach (Button b in buttons)
+			{
+				b.Text = "";
+				b.BackgroundColor = Color.Blue;
+			}
+		}
+	}
+}
